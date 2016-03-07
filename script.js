403b564308a68d985a58311f13f5ece70ee93596
mnyeks <0719075643kennyg@gmail.com>
2016-03-07 11:16:49 +0200

    commiting

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript code goes here at the bottom
+		// Returns a random number between min (inclusive) and max (exclusive)
+		// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+	  }
+	  document.getElementById("Rand").innerHTML = getRandomIntInclusive(1, 3);
+	  document.getElementById("Rand1").innerHTML = getRandomIntInclusive(1, 3);
+	  document.getElementById("Rand2").innerHTML = getRandomIntInclusive(1, 3);
